cab0f7d6b6fc4dea120ea93835762b2272486dac
ts666823 <56662992+ts666823@users.noreply.github.com>
2020-05-30 23:31:54 +0800

    Pop Sprite

---
+++ PopStar Sprite/ThePopSprite.cpp
@@ -0,0 +1,86 @@
+#include "ThePopSprite.h"
+USING_NS_CC;
+PopSprite* PopSprite::createPopSprite(int color, int width, int height, float popSpriteX, float popSpriteY) {
+	PopSprite* myPrivate = new PopSprite();
+	if (myPrivate && myPrivate->init())
+	{
+		//自动释放
+		myPrivate->autorelease();
+		//生成对象
+		myPrivate->privateInit(color, width, height, popSpriteX, popSpriteY);
+		return myPrivate;
+	}
+	//如果生成enemy对象失败
+	//使用delete操作符删除一个对象，如果对象为NULL，则不进行操作
+	CC_SAFE_DELETE(myPrivate);
+	return NULL;
+}
+
+bool PopSprite::init() {
+	if (!Sprite::init())
+	{
+		return false;
+	}
+	return true;
+}
+
+bool PopSprite::privateInit(int color, int width, int height, int popSpriteX, int popSpriteY)
+{
+	//初始化
+	iColor = color;
+	popX = 0;
+	popY = 0;
+	//颜色的初始化
+	ColorBlock = LayerColor::create(Color4B(248, 248, 255,200),width-5,height-5);
+	ColorBlock->setPosition(Vec2(popSpriteX, popSpriteY));
+	this->addChild(ColorBlock);
+	return true;
+}
+
+int PopSprite::getColor()
+{
+	return iColor;
+}
+
+bool PopSprite::setColor(int colors)
+{
+	iColor = colors;
+	switch (iColor)
+	{
+	case 1:ColorBlock->setColor(Color3B(220, 20, 60)); break;
+	case 2:ColorBlock->setColor(Color3B(0, 0, 205)); break;
+	case 3:ColorBlock->setColor(Color3B(46, 139, 87)); break;
+	case 1:ColorBlock->setColor(Color3B(220, 20, 60)); break;
+	default:
+		return false;
+	}
+	return true;
+}
+
+int PopSprite::getPopX()
+{
+	return popX;
+}
+
+int PopSprite::getPopY()
+{
+	return popY;
+}
+
+bool PopSprite::setPopX(int x)
+{
+	popX = x;
+}
+
+bool PopSprite::setPopY(int y)
+{
+	popY = y;
+}
+
+Point PopSprite::getPoint()
+{
+	return ColorBlock->getPosition();
+}
+
+
+
---
+++ PopStar Sprite/ThePopSprite.h
@@ -0,0 +1,33 @@
+#include "cocos2d.h"
+USING_NS_CC;
+class PopSprite : public Sprite{
+	//初始化游戏的数值，宽度，高度，x轴，y轴
+	
+public:
+	static PopSprite* createPopSprite(int color,int width,int height,float popSpriteX,float popSpriteY);
+	virtual bool init();
+	//获取颜色的函数
+	int getColor();
+	//设置颜色的函数
+	bool setColor(int colors);
+	//获取X,Y坐标的函数
+	int getPopX();
+	int getPopY();
+	//设置X,Y坐标的函数
+	bool setPopX(int x);
+	bool setPopY(int y);
+	//获取某个PopSprite位置的函数
+	Point getPoint();
+	//PopSprite的坐标
+	int popX, popY;
+	//宏定义，内存管理
+	CREATE_FUNC(PopSprite);
+private:
+	//颜色
+	int iColor;
+	//私有化的初始方法
+	bool privateInit(int color,int width, int height, int popSpriteX, int popSpriteY);
+	//显示的颜色
+	LayerColor* ColorBlock;
+};
+
